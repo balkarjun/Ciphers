41a843dd439405f7f937aa2852243fe8231c21e6
Arjun Balakrishnan <balkarjun@gmail.com>
2023-04-05 21:37:37 +0530

    add PigpenCharacter

---
+++ Pigpen/PigpenCharacter.swift
@@ -0,0 +1,147 @@
+//
+//  PigpenCharacter.swift
+//  
+//
+//  Created by Arjun B on 05/04/23.
+//
+
+import SwiftUI
+
+enum PigDirection {
+    case top, right, bottom, left
+}
+
+func isPigSquare(_ character: String) -> Bool {
+    let squarePigpenCharacters: Set<String> = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R"]
+    
+    return squarePigpenCharacters.contains(character)
+}
+
+func isPigDotted(_ character: String) -> Bool {
+    let dottedCharacters: Set<String> = ["J", "K", "L", "M", "N", "O", "P", "Q", "R", "W", "X", "Y", "Z"]
+    
+    return dottedCharacters.contains(character)
+}
+
+let PigDirections: [String : Set<PigDirection>] = [
+    "A": [.right, .bottom],
+    "B": [.left, .bottom, .right],
+    "C": [.left, .bottom],
+    "D": [.top, .right, .bottom],
+    "E": [.top, .right, .bottom, .left],
+    "F": [.top, .left, .bottom],
+    "G": [.top, .right],
+    "H": [.left, .top, .right],
+    "I": [.left, .top],
+    "J": [.right, .bottom],
+    "K": [.left, .bottom, .right],
+    "L": [.left, .bottom],
+    "M": [.top, .right, .bottom],
+    "N": [.top, .right, .bottom, .left],
+    "O": [.top, .left, .bottom],
+    "P": [.top, .right],
+    "Q": [.left, .top, .right],
+    "R": [.left, .top],
+    "S": [.bottom],
+    "T": [.right],
+    "U": [.left],
+    "V": [.top],
+    "W": [.bottom],
+    "X": [.right],
+    "Y": [.left],
+    "Z": [.top],
+]
+
+struct PigpenCharacter: View {
+    private let character: String
+    private let lineColor: Color
+    
+    init(_ character: String, lineColor: Color = .black) {
+        self.character = character.uppercased()
+        self.lineColor = lineColor
+    }
+    
+    private var isSquare: Bool {
+        isPigSquare(self.character)
+    }
+    
+    private var directions: Set<PigDirection> {
+        PigDirections[self.character] ?? [.right, .bottom]
+    }
+    
+    private var dotted: Bool {
+        isPigDotted(self.character)
+    }
+    
+    private var angularOffset: Double {
+        switch (directions.first ?? .top) {
+        case .top:    return 0
+        case .right:  return 90
+        case .bottom: return 180
+        case .left:   return 270
+        }
+    }
+    
+    private let lineWidth: Double = 2
+    private let dotSize: Double = 4
+    
+    var body: some View {
+        Rectangle()
+            .fill(.clear)
+            .frame(width: 20, height: 20)
+            .overlay {
+                ZStack {
+                    Rectangle()
+                        .fill(lineColor)
+                        .frame(height: lineWidth)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                        .opacity(directions.contains(.top) ? 1 : 0)
+                    
+                    Rectangle()
+                        .fill(lineColor)
+                        .frame(width: lineWidth)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .trailing)
+                        .opacity(directions.contains(.right) ? 1 : 0)
+                    
+                    Rectangle()
+                        .fill(lineColor)
+                        .frame(height: lineWidth)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                        .opacity(directions.contains(.bottom) ? 1 : 0)
+                    
+                    Rectangle()
+                        .fill(lineColor)
+                        .frame(width: lineWidth)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                        .opacity(directions.contains(.left) ? 1 : 0)
+                }
+                .opacity(isSquare ? 1 : 0)
+            }
+            .overlay {
+                ZStack {
+                    RoundedRectangle(cornerRadius: .infinity)
+                        .fill(lineColor)
+                        .frame(width: lineWidth)
+                        .rotationEffect(.degrees(29), anchor: .top)
+                    RoundedRectangle(cornerRadius: .infinity)
+                        .fill(lineColor)
+                        .frame(width: lineWidth)
+                        .rotationEffect(.degrees(-29), anchor: .top)
+                }
+                .rotationEffect(.degrees(angularOffset))
+                .opacity(!isSquare ? 1 : 0)
+            }
+            .overlay {
+                Circle()
+                    .fill(lineColor)
+                    .frame(width: dotSize, height: dotSize)
+                    .opacity(dotted ? 1 : 0)
+            }
+    }
+}
+
+struct PigpenCharacter_Previews: PreviewProvider {
+    static var previews: some View {
+        PigpenCharacter("E")
+    }
+}
