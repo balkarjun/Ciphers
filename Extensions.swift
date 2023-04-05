bb68d5dfd29c123a74208e2c882a7bcf24ba76d9
Arjun Balakrishnan <balkarjun@gmail.com>
2023-04-05 21:39:49 +0530

    add array chunk extension

--- Extensions.swift
+++ Extensions.swift
@@ -17,6 +17,14 @@ extension View {
     }
 }
 
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
+
 struct SizePreferenceKey: PreferenceKey {
   static var defaultValue: CGSize = .zero
 
