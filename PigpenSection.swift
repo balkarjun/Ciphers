2a32c7d908ffe26c06c1dddedd6187d0e0d582ed
Arjun Balakrishnan <balkarjun@gmail.com>
2023-04-06 23:45:22 +0530

    show larger preview of highlighted character

--- PigpenSection.swift
+++ PigpenSection.swift
@@ -16,88 +16,100 @@ struct PigpenSection: View {
     
     var body: some View {
         ScrollView {
-            VStack {                    
+            VStack {
+                HStack(spacing: 16) {
+                    PigpenCharacter(String(highlighted), lineColor: .green, charWidth: 80, charHeight: 80)
+                        .padding()
+                        .padding(.leading)
+                        .opacity(highlighted == "" ? 0 : 1)
+                    
+                    Rectangle()
+                        .fill(.quaternary)
+                        .frame(width: 2)
+                    
                     PigpenText(
                         target,
                         highlighted: highlighted,
                         replaced: Set<String>()
                     )
                     .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 8, style: .continuous)
-                            .strokeBorder(.quaternary.opacity(0.5), lineWidth: 1)
-                    }
-                    PigpenText(
-                        target,
-                        highlighted: "",
-                        replaced: completedCharacters,
-                        lineColor: .clear
-                    )
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 8, style: .continuous)
-                            .strokeBorder(.quaternary.opacity(0.5), lineWidth: 1)
-                    }
-                    .padding(.bottom)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                        .strokeBorder(.quaternary.opacity(0.5), lineWidth: 1)
+                }
+                
+                PigpenText(
+                    target,
+                    highlighted: "",
+                    replaced: completedCharacters,
+                    lineColor: .clear
+                )
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                        .strokeBorder(.quaternary.opacity(0.5), lineWidth: 1)
+                }
+                .padding(.bottom)
                     
-                    HStack(spacing: 24) {
-                        PigpenSquareKeyboard(
+                HStack(spacing: 24) {
+                    PigpenSquareKeyboard(
+                        tapped: $tapped,
+                        characters: ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+                    )
+                    
+                    VStack(spacing: 24) {
+                        PigpenTriangleKeyboard(
                             tapped: $tapped,
-                            characters: ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+                            characters: ["S", "T", "U", "V"]
                         )
                         
-                        VStack(spacing: 24) {
-                            PigpenTriangleKeyboard(
-                                tapped: $tapped,
-                                characters: ["S", "T", "U", "V"]
-                            )
-                            
-                            PigpenTriangleKeyboard(
-                                tapped: $tapped,
-                                characters: ["W", "X", "Y", "Z"],
-                                dotted: true
-                            )
-                        }
-                        
-                        PigpenSquareKeyboard(
+                        PigpenTriangleKeyboard(
                             tapped: $tapped,
-                            characters: ["J", "K", "L", "M", "N", "O", "P", "Q", "R"],
+                            characters: ["W", "X", "Y", "Z"],
                             dotted: true
                         )
                     }
                     
-                    Spacer()
+                    PigpenSquareKeyboard(
+                        tapped: $tapped,
+                        characters: ["J", "K", "L", "M", "N", "O", "P", "Q", "R"],
+                        dotted: true
+                    )
                 }
-                .padding()
-                .onChange(of: tapped) { _ in
-                    // check if the correct character was entered
-                    if tapped != highlighted {
-                        tapped = ""
-                        return
-                    }
-                    
-                    // add the character to the completed set
-                    completedCharacters.insert(tapped)
-                    
-                    // skip until next unseen character is reached. highlight that
-                    var found = false
-                    for character in target.map({ String($0) }) {
-                        if !completedCharacters.contains(character) {
-                            highlighted = character
-                            found = true
-                            break
-                        }
-                    }
-                    
-                    // if all characters were skipped, the solution is complete
-                    if !found {
-                        highlighted = ""
-                    }
-                    
+                
+                Spacer()
+            }
+            .padding()
+            .onChange(of: tapped) { _ in
+                // check if the correct character was entered
+                if tapped != highlighted {
                     tapped = ""
+                    return
                 }
+                
+                // add the character to the completed set
+                completedCharacters.insert(tapped)
+                
+                // skip until next unseen character is reached. highlight that
+                var found = false
+                for character in target.map({ String($0) }) {
+                    if !completedCharacters.contains(character) {
+                        highlighted = character
+                        found = true
+                        break
+                    }
+                }
+                
+                // if all characters were skipped, the solution is complete
+                if !found {
+                    highlighted = ""
+                }
+                
+                tapped = ""
+            }
             .navigationBarTitleDisplayMode(.inline)
         }
     }
