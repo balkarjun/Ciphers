f1f8efb0b838ae7bac8e4416156e223331147c4c
Arjun Balakrishnan <balkarjun@gmail.com>
2023-04-09 00:59:18 +0530

    add basic page navigation

--- ContentView.swift
+++ ContentView.swift
@@ -1,7 +1,100 @@
 import SwiftUI
 
+enum AppPage: Int, CaseIterable {
+    case welcome = 0
+    case pigpenInfo = 1
+    case pigpenCipher = 2
+    case grilleInfo = 3
+    case grilleCipher = 4
+    case final = 5
+}
+
 struct ContentView: View {
+    @State private var currentPage: AppPage = .welcome
+    private var isFirstPage: Bool {
+        currentPage.rawValue <= 0
+    }
+    
+    private var isLastPage: Bool {
+        currentPage.rawValue >= AppPage.allCases.count - 1
+    }
+    
+    func nextPage() {
+        if isLastPage { return }
+        
+        currentPage = AppPage(rawValue: currentPage.rawValue + 1) ?? .welcome
+    }
+    
+    func prevPage() {
+        if isFirstPage { return }
+        
+        currentPage = AppPage(rawValue: currentPage.rawValue - 1) ?? .welcome
+    }
+    
+    private var pageTitle: String {
+        switch currentPage {
+        case .welcome:
+            return "Welcome"
+        case .pigpenInfo:
+            return "Pigpen Info"
+        case .pigpenCipher:
+            return "Pigpen Cipher"
+        case .grilleInfo:
+            return "Grille Info"
+        case .grilleCipher:
+            return "Grille Cipher"
+        case .final:
+            return "Final"
+        }
+    }
+    
     var body: some View {
-        Pigpen()
+        VStack {
+            HStack {
+                Button(action: prevPage) {
+                    HStack {
+                        Image(systemName: "chevron.left")
+                        Text("Previous")
+                    }
+                }
+                .buttonStyle(.bordered)
+                .tint(.accentColor)
+                .disabled(isFirstPage)
+                
+                Text(pageTitle)
+                    .padding(.vertical, 8)
+                    .frame(maxWidth: .infinity)
+                    .background(.quaternary.opacity(0.5))
+                    .cornerRadius(6)
+                
+                Button(action: nextPage) {
+                    HStack {
+                        Text("Next")
+                        Image(systemName: "chevron.right")
+                    }
+                }
+                .buttonStyle(.bordered)
+                .tint(.accentColor)
+                .disabled(isLastPage)
+            }
+            .padding(.horizontal)
+            
+            ScrollView {
+                switch currentPage {
+                case .welcome:
+                    EmptyView()
+                case .pigpenInfo:
+                    EmptyView()
+                case .pigpenCipher:
+                    PigpenSection()
+                case .grilleInfo:
+                    EmptyView()
+                case .grilleCipher:
+                    EmptyView()
+                case .final:
+                    EmptyView()
+                }
+            }
+        }
     }
 }
--- PigpenSection.swift
+++ PigpenSection.swift
@@ -15,105 +15,103 @@ struct PigpenSection: View {
     let target = "Viewed through holes of light, hidden in plain sight, the clue faces left from right".uppercased()
     
     var body: some View {
-        ScrollView {
-            VStack {
-                HStack(spacing: 16) {
-                    PigpenCharacter(
-                        String(highlighted),
-                        charWidth: 80,
-                        charHeight: 80
-                    )
-                    .padding()
-                    .padding(.leading)
-                    .opacity(highlighted == "" ? 0 : 1)
-                    
-                    Rectangle()
-                        .fill(.quaternary.opacity(0.5))
-                        .frame(width: 2)
-                    
-                    PigpenText(
-                        target,
-                        highlighted: highlighted,
-                        completed: completedCharacters
-                    )
-                    .padding()
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 8, style: .continuous)
-                        .strokeBorder(.quaternary.opacity(0.5), lineWidth: 2)
-                }
+        VStack {
+            HStack(spacing: 16) {
+                PigpenCharacter(
+                    highlighted,
+                    charWidth: 80,
+                    charHeight: 80
+                )
+                .padding()
+                .padding(.leading)
+                .opacity(highlighted == "" ? 0 : 1)
                 
-                AnswerField(
-                    target: target,
+                Rectangle()
+                    .fill(.quaternary.opacity(0.5))
+                    .frame(width: 2)
+                
+                PigpenText(
+                    target,
+                    highlighted: highlighted,
                     completed: completedCharacters
                 )
                 .padding()
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 8, style: .continuous)
-                        .strokeBorder(.quaternary.opacity(0.5), lineWidth: 2)
-                }
-                .padding(.bottom)
-                    
-                HStack(spacing: 24) {
-                    PigpenSquareKeyboard(
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .overlay {
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .strokeBorder(.quaternary.opacity(0.5), lineWidth: 2)
+            }
+            
+            AnswerField(
+                target: target,
+                completed: completedCharacters
+            )
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .overlay {
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .strokeBorder(.quaternary.opacity(0.5), lineWidth: 2)
+            }
+            .padding(.bottom)
+                
+            HStack(spacing: 24) {
+                PigpenSquareKeyboard(
+                    tapped: $tapped,
+                    characters: ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+                )
+                
+                VStack(spacing: 24) {
+                    PigpenTriangleKeyboard(
                         tapped: $tapped,
-                        characters: ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+                        characters: ["S", "T", "U", "V"]
                     )
                     
-                    VStack(spacing: 24) {
-                        PigpenTriangleKeyboard(
-                            tapped: $tapped,
-                            characters: ["S", "T", "U", "V"]
-                        )
-                        
-                        PigpenTriangleKeyboard(
-                            tapped: $tapped,
-                            characters: ["W", "X", "Y", "Z"],
-                            dotted: true
-                        )
-                    }
-                    
-                    PigpenSquareKeyboard(
+                    PigpenTriangleKeyboard(
                         tapped: $tapped,
-                        characters: ["J", "K", "L", "M", "N", "O", "P", "Q", "R"],
+                        characters: ["W", "X", "Y", "Z"],
                         dotted: true
                     )
                 }
                 
-                Spacer()
+                PigpenSquareKeyboard(
+                    tapped: $tapped,
+                    characters: ["J", "K", "L", "M", "N", "O", "P", "Q", "R"],
+                    dotted: true
+                )
             }
-            .padding()
-            .onChange(of: tapped) { _ in
-                // check if the correct character was entered
-                if tapped != highlighted {
-                    tapped = ""
-                    return
-                }
-                
-                // add the character to the completed set
-                completedCharacters.insert(tapped)
-                
-                // skip until next unseen character is reached. highlight that
-                var found = false
-                for character in target.map({ String($0) }) {
-                    if !completedCharacters.contains(character) {
-                        highlighted = character
-                        found = true
-                        break
-                    }
-                }
-                
-                // if all characters were skipped, the solution is complete
-                if !found {
-                    highlighted = ""
-                }
-                
-                tapped = ""
-            }
-            .navigationBarTitleDisplayMode(.inline)
+            
+            Spacer()
         }
+        .padding()
+        .onChange(of: tapped) { _ in
+            // check if the correct character was entered
+            if tapped != highlighted {
+                tapped = ""
+                return
+            }
+            
+            // add the character to the completed set
+            completedCharacters.insert(tapped)
+            
+            // skip until next unseen character is reached. highlight that
+            var found = false
+            for character in target.map({ String($0) }) {
+                if !completedCharacters.contains(character) {
+                    highlighted = character
+                    found = true
+                    break
+                }
+            }
+            
+            // if all characters were skipped, the solution is complete
+            if !found {
+                highlighted = ""
+            }
+            
+            tapped = ""
+        }
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
