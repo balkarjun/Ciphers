57573084fcb7e8a578b583863fb2072b09cfed71
Arjun Balakrishnan <balkarjun@gmail.com>
2023-04-14 16:57:24 +0530

    add grille page explanations

--- GrillePage.swift
+++ GrillePage.swift
@@ -15,7 +15,7 @@ struct GrillePage: View {
 
     var body: some View {
         HStack(spacing: 16) {
-            VStack {
+            VStack(alignment: .leading, spacing: 0) {
                 HStack {
                     Image(systemName: "3.circle.fill")
                         .font(.body.weight(.bold))
@@ -29,17 +29,46 @@ struct GrillePage: View {
                 .background(.mint.opacity(0.15))
                 .cornerRadius(8)
                 
-                Text(target)
-                    .font(.title2.monospaced())
-                    .fontWeight(.medium)
-                    .padding()
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(.ultraThinMaterial)
-                    .cornerRadius(8)
-                
-                
-                Spacer()
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Text(target)
+                            .font(.title3.monospaced())
+                            .fontWeight(.medium)
+                            .padding()
+                            .padding()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(.ultraThinMaterial)
+                            .cornerRadius(8)
+                        
+                        Text("In a ***Grille Cipher***, the message is hidden in plain sight, and is visible when viewed through a stencil or piece of paper with holes in it.")
+                            .padding([.horizontal, .top])
+                        
+                        
+                        VStack(alignment: .leading, spacing: 6) {
+                            HStack {
+                                Image(systemName: "rectangle.and.hand.point.up.left.fill")
+                                    .symbolRenderingMode(.hierarchical)
+                                    .foregroundColor(.teal)
+                                    .font(.body.bold())
+                                
+                                Text("Find the Hidden Key")
+                                    .font(.subheadline.weight(.semibold))
+                                    .foregroundColor(.teal)
+                                    .offset(y: -1)
+                            }
+                            Text("Align the stencil with the text to uncover the secret key.")
+                            
+                            Text("***Hint***: Make sure you read the message. It holds the clue as to how to decode the message.")
+                        }
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 8)
+                                .strokeBorder(.quaternary, lineWidth: 1)
+                        }
+                    }
+                    .padding(.top, 12)
+                }
             }
             .frame(maxWidth: .infinity)
             
